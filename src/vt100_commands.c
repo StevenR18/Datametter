b2fc78895d9ecfa04a6a8f0698392f9b3b887f32
Steven <steven.riofrio.villamar@gmail.com>
2025-02-05 09:53:05 -0500

    se agrego funcion2

---
+++ src/vt100_commands.c
@@ -0,0 +1,18 @@
+/*
+  ESTE ARCHIVO CONTENDRA LOS COMANDOS DE UNA TERMINAL VIRUTAL VT100
+  ECHO "S" muestra una cadena en la terminal
+  CLS      limpia la pantalla
+  ..
+  etc
+ */
+
+int funcion()
+{
+  int x=0;
+  return 0;
+}
+
+int suma(int x, int y)
+{
+  return (x+y);
+}
