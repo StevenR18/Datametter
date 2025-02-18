d5b87594812e3dd6a59005ef84c126bf721aedad
Steven <steven.riofrio.villamar@gmail.com>
2025-02-18 09:50:08 -0500

    se modificaron los prototipo de algunas funciones

--- includes/main_datametter.h
+++ includes/main_datametter.h
@@ -2,8 +2,30 @@
 #define MAIN_DATAMETTER_H
 #include "..\..\Vt\includes\window64_platform.h"
 #include "..\..\Vt\includes\keyboard.h"
+#include "..\..\Vt\includes\liked_list.h"
+#include "..\..\Vt\includes\row.h"
+
+
+
+typedef struct
+{
+  POINT vCursor;
+  Nodo *buffercomand;
+  int  width;
+  int  height;
+  int pixelPerChar;
+  Row *row; // array del buffe principal
+  int capRow;
+  int ofsetY; // se utiliza para hacer escroll vertical
+  int ofsetX; // se utilzia pra hacer escroll horizontal
+  int ofsetCapRow; // ofset para la capacidad del cursor 
+}View;                  
+
+extern View vEmu;
+
 void main_app(Win32ScreenBuffer *Buffer, KeyboardEvent * k);
-void inic_app();
+void inic_app(Win32WindowDimension Dim);
 void end_app();
-void appendRow(const wchar_t * s, int len);
+void updateDim(int w, int h);
+
 #endif
